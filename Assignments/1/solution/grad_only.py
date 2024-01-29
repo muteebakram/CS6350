2681eeaa9e8e348282a0395cc321d7e0f048f0bc
Muteeb Akram <muteebakram@gmail.com>
2024-01-29 01:22:05 -0700

    complete grad only 1

--- Assignments/1/solution/grad_only.py
+++ Assignments/1/solution/grad_only.py
@@ -30,9 +30,9 @@ def get_info_gain(df, attr, attr_values):
     total_samples = df.shape[0]
     total_entropy = get_total_entropy(df)
     # print(f"total_entropy: {total_entropy}")
+    # print(attr, attr_values)
 
     for attr_value in attr_values:
-        # print(attr, attr_value)
         sub_df = get_data_frame_subset(df, attribute=attr, attribute_value=attr_value)
         # print(sub_df)
         samples = sub_df.shape[0]
@@ -56,7 +56,7 @@ def calculate_info_gain_T(df):
         info_gain_t[attr] = (gain * gain) / cost
 
     for attr, gain in info_gain_t.items():
-        print(f"Attribute: {attr:<10} GainT:  {gain:<25}")
+        print(f"Attribute: {attr:<10} GainT: {gain:<25} (Round upto 3 - {round(gain, 3)})")
 
     return info_gain_t
 
@@ -68,7 +68,7 @@ def calculate_info_gain_S(df):
         info_gain_s[attr] = (pow(2, gain) - 1) / (math.sqrt(cost + 1))
 
     for attr, gain in info_gain_s.items():
-        print(f"Attribute: {attr:<10} GainS:  {gain:<25}")
+        print(f"Attribute: {attr:<10} GainS: {gain:<25} (Round upto 3 - {round(gain, 3)})")
 
     return info_gain_s
 
@@ -81,14 +81,16 @@ def get_max_dict_value(dictionary):
 
 if __name__ == "__main__":
     df = pd.read_csv("./data/attribute_cost.csv")
-    # print(df)
 
-    # print(get_info_gain(df, "shape", list(df["shape"].unique())))
+    print(
+        """1. [8 points] Compute the modified gains GainT and GainS for each attribute using these costs. Fill in your results in the table below. (upto 3 decimal places)"""
+    )
     print("GainT (S, A)")
     info_gain_t = calculate_info_gain_T(df)
 
     print("\nGainS (S, A)")
     info_gain_s = calculate_info_gain_S(df)
 
-    print(f"\nBest GainT feature for root of the decision tree is '{get_max_dict_value(info_gain_t)}'")
+    print("\n2. [2 points] For each variant of gain, which feature would you choose as the root?")
+    print(f"Best GainT feature for root of the decision tree is '{get_max_dict_value(info_gain_t)}'")
     print(f"Best GainS feature for root of the decision tree is '{get_max_dict_value(info_gain_s)}'\n")
